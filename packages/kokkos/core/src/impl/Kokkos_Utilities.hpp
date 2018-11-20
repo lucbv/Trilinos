8397fa271000ffc5005bdd347299fdd019f9b6af
Luc Berger-Vergiat <lberge@sandia.gov>
2018-11-20 14:03:19 -0700

    Snapshot of kokkos.git from commit 564ec79e3409f9bdd6d1e9a6cadad047764cd816
    From repository at git@github.com:kokkos/kokkos.git
    
    At commit:
    commit 564ec79e3409f9bdd6d1e9a6cadad047764cd816
    Merge: 7a06fc8 9614f72
    Author: Dan Ibanez <ibaned@users.noreply.github.com>
    Date:   Tue Nov 6 14:27:47 2018 -0700
    
        Merge pull request #1884 from kokkos/master-backmerge
    
        Update master-history

--- packages/kokkos/core/src/impl/Kokkos_Utilities.hpp
+++ packages/kokkos/core/src/impl/Kokkos_Utilities.hpp
@@ -409,9 +409,6 @@ struct inclusive_scan_integer_sequence
   static constexpr value_type value  = helper::value ;
 };
 
-template <typename T>
-using identity_t = T;
-
 }} // namespace Kokkos::Impl
 
 
